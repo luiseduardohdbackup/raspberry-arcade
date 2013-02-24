0c0573e693f768d6c9602a8976ae44efff83407c
Pedro Sánchez <pedro@xanchez.com>
2013-02-24 00:35:19 -0600

    Using python-uinput instead of autopy

--- arcade.py
+++ arcade.py
@@ -1,5 +1,5 @@
 import smbus
-import autopy
+import uinput
 import time
 
 ( IODIRA, 
@@ -23,24 +23,28 @@ import time
   GPIOA, 
   GPIOB,
   OLATA,
-  OLATB) = range(0x00, 0x16)
+  OLATB ) = range(0x00, 0x16)
 
 PRESSED_VALUE = 0
 
 devices = [0x20, 0x21, 0x22]	
 keystrokes = []
-keys = [ autopy.key.K_UP, autopy.key.K_LEFT,
-         autopy.key.K_RIGHT,autopy.key.K_DOWN,
-         'a',  'b',  'c', 'd',  'e', 'f',  'g',  
-         'h',  'i',  'j', 'k',  'l', 'm',  'n',
-         'o',  'p',  'q', 'r',  's', 't',  'u',
-         'v',  'w',  'x', 'y',  'z', '0',  '1',
-         '2',  '3',  '4', '5',  '6', '7',  '8',
-         '9',  autopy.key.K_PAGEDOWN, 
-         autopy.key.K_DELETE, autopy.key.K_HOME, 
-         autopy.key.K_F12, autopy.key.K_F11, 
-         autopy.key.K_F10, autopy.key.K_F9, 
-         autopy.key.K_F8 ]
+keys = [ uinput.KEY_UP, uinput.KEY_LEFT, uinput.KEY_RIGHT,
+         uinput.KEY_DOWN, uinput.KEY_A, uinput.KEY_B,
+         uinput.KEY_C, uinput.KEY_D, uinput.KEY_E, uinput.KEY_F,
+         uinput.KEY_G, uinput.KEY_H, uinput.KEY_I, uinput.KEY_J,
+         uinput.KEY_K, uinput.KEY_L, uinput.KEY_M, uinput.KEY_N,
+         uinput.KEY_O, uinput.KEY_P, uinput.KEY_Q, uinput.KEY_R,
+         uinput.KEY_S, uinput.KEY_T, uinput.KEY_U, uinput.KEY_V,
+         uinput.KEY_W, uinput.KEY_X, uinput.KEY_Y, uinput.KEY_Z,
+         uinput.KEY_0, uinput.KEY_1, uinput.KEY_2, uinput.KEY_3,
+         uinput.KEY_4, uinput.KEY_5, uinput.KEY_6, uinput.KEY_7,
+         uinput.KEY_8, uinput.KEY_9, uinput.KEY_PAGEDOWN, 
+         uinput.KEY_DELETE, uinput.KEY_HOME, uinput.KEY_F12,
+         uinput.KEY_F11, uinput.KEY_F10, uinput.KEY_F9,
+         uinput.KEY_F8 ]
+
+keyboard = uinput.Device(keys)
 
 I2C = smbus.SMBus(1)
 
@@ -63,9 +67,9 @@ while True:
 			keystrokes.append(keys[button])
 	
 	for keystroke in keystrokes:
-		autopy.key.toggle(keystroke, True)
+		keyboard.emit(keystroke, 1)
 	
 	time.sleep(0.1)
 	
 	for keystroke in keystrokes:
-		autopy.key.toggle(keystroke, False)
+		keyboard.emit(keystroke, 0)
